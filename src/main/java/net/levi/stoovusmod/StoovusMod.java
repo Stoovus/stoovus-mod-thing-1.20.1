8b47fb55d495c65f6b0239d962e67f7dfbd2ac33
Stoovus <levistovall777@gmail.com>
2025-02-27 17:58:59 -0800

    Test

---
+++ src/main/java/net/levi/stoovusmod/StoovusMod.java
@@ -0,0 +1,18 @@
+package net.levi.stoovusmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StoovusMod implements ModInitializer {
+	public static final String MOD_ID = "stoovusmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/levi/stoovusmod/StoovusModClient.java
@@ -0,0 +1,10 @@
+package net.levi.stoovusmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StoovusModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
